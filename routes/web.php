c2aab411811175f1813c4e6e497a26b3d4feb685
MeraryP <dpinedam@unah.hn>
2023-02-13 09:20:48 -0600

    add: routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EstudianteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/egresados',[EgresadoController::class, 'index'])->name('egresado.index');
