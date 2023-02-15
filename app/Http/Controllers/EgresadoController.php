8f6e5cb41ab4b9d664ace659e1682be87904d317
ada-rodriguez <alrodriguezp@unah.hn>
2023-02-14 22:34:20 -0600

    H5

---
+++ app/Http/Controllers/EgresadoController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Egresado;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class EgresadoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $texto=trim($request->get('texto'));
+        $egresados =DB::table('egresados')
+        ->select('id','identidad','nombre','fecha_nacimiento','genero','carreras','año_egresado')
+        ->where('nombre' ,'LIKE','%'.$texto.'%')
+        ->orwhere('identidad' ,'LIKE','%'.$texto.'%')
+        ->paginate(5);
+        return view ('egresado/index',compact('egresados', 'texto'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $egresado = Egresado::find($id);
+        $egresado->delete();
+        return redirect('/egresados')->with('mensaje', 'Egresado fue borrado completamente');
+    }
+}
