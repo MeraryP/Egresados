9a1a79b85d54b753064590de09fbf20355b06432
ada-rodriguez <118467600+ada-rodriguez@users.noreply.github.com>
2023-02-20 13:45:21 -0600

    User seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;    
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create(
+            [
+                'username' => 'admin13790tds',
+                'name' => 'cosme',
+                'correo' => 'cosme@gmail.com',
+                'nacimiento' => '19990909',
+                'identidad' => '0000000000000',
+                'telefono' => '00000000',
+                'password' => bcrypt('cosme13790'),
+            ]
+        )->assignRole('Admin');
+
+        User::create(
+            [
+                'name' => 'cosme',
+                'correo' => 'cosme2@gmail.com',
+                'nacimiento' => '19990909',
+                'identidad' => '0000000000001',
+                'telefono' => '00000001',
+                'username' => 'Martha Sanchez',
+                'password' => bcrypt('cosmegc1976'),
+            ]
+        )->assignRole('Ordinario');
+    }
+}
