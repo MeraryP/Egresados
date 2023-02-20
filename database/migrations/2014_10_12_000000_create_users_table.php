95295d2e4977b751af9319a8b6edc0acda0390b6
MeraryP <118649149+MeraryP@users.noreply.github.com>
2023-02-20 13:46:25 -0600

    tabla de usuario

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,13 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('correo');
+            $table->date('nacimiento');
+            $table->string('username');
             $table->string('password');
+            $table->string('identidad');
+            $table->string('telefono');
+            $table->boolean('estado')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
