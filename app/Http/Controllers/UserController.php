4f723e127d68a6a88c9d74dbf692f06b63652e92
Karla20Mairena <118698801+Karla20Mairena@users.noreply.github.com>
2023-02-20 13:34:09 -0300

    Datos

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -27,4 +27,47 @@ class UserController extends Controller
     {
         return view ('User.clave');
     }
+/**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function guardarclave(Request $request)
+    {
+
+        $rules=[
+            'viejapassword' => ["required",new Contraactual],
+            'password' => 'required|min:8|confirmed',
+        ];
+
+        $mensaje=[
+            'viejapassword.required' => 'La contraseña no puede estar vacío',
+            'viejapassword.Contractual' => 'La contraseña no coincide',
+            'password.required' => 'La contraseña no puede estar vacío',
+            'password.min' => 'La contraseña debe de tener mas de 8 caracteres',
+            'password.confirmed' => 'La contraseña no coinciden',
+        ];
+
+        $this->validate($request,$rules,$mensaje);
+
+
+        $contra = User::findOrFail(auth()->user()->id);
+        $contra->password = Hash::make($request->input('password'));
+
+        $contra->save();
+
+        if($contra){
+            return redirect('/')->with('mensaje', 'La contraseña fue actualizada exitosamente.');
+        }else{
+            //retornar con un mensaje de error.
+        }
+
+    }
+
+    public function usuario(){
+        return view('User.datos');
+    }
+
+
 }
