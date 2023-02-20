53b76f857bbf9fffb8dcda79fae6c91c3e421f93
Karla20Mairena <118698801+Karla20Mairena@users.noreply.github.com>
2023-02-20 13:05:35 -0300

    Rutas

--- routes/web.php
+++ routes/web.php
@@ -16,22 +16,56 @@ use App\Http\Controllers\Controller;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware("auth")->group(function () {
+    Route::middleware("desactivado")->group(function () {
+    
+    
+        Route::get('/', function () {
+            return view('welcome');
+        })->name('welcome');
+
+        Route::resource('carreras', 'App\Http\Controllers\CarreraController');
+
+       Route::resource('/egresado', 'App\Http\Controllers\EgresadoController');
+
+        Route::put('/egresado/{id}/editar', [EgresadoController::class, 'update'])
+        ->name('egresado.update')->where('id','[0-9]+');
+
+        Route::put('/carreras/{id}/editar', [CarreraController::class, 'update'])
+       ->name('carrera.update')->where('id','[0-9]+');
+        
+    
+        //ruta  formulario
+        Route::get('/contrasenia',[UserController::class, 'formularioclave'])
+        ->name('contrasenia.cambiar');
+    
+        //ruta guardar
+        Route::post('/contrasenia',[UserController::class, 'guardarclave'])
+            ->name('contrasenia.cambiada');
+    
+        //ruta  formulario
+        Route::get('/registrar',[UserController::class, 'registrar'])
+        ->name('usuario.registrar');
+    
+        //ruta guardar
+        Route::post('/registrar',[UserController::class, 'guardar'])->name('usuario.guardar');
+    
+        Route::get('/usuario',[UserController::class, 'usuario'])
+        ->name('usuario.datos');
+    
+        Route::get('/listadousuario',[UserController::class, 'listado'])
+        ->name('usuario.listado');
+    
+        Route::get('/usuario/desactivar/{id}',[UserController::class, 'desactivar'])
+        ->name('user.desactivar');
+    
+        Route::get('/usuario/activar/{id}',[UserController::class, 'activar'])
+        ->name('user.activar');
+        Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+    });
+    
 });
 
-
-Route::resource('carreras', 'App\Http\Controllers\CarreraController');
-
-Route::resource('/egresado', 'App\Http\Controllers\EgresadoController');
-
-Route::put('/egresado/{id}/editar', [EgresadoController::class, 'update'])
-->name('egresado.update')->where('id','[0-9]+');
-
-Route::put('/carreras/{id}/editar', [CarreraController::class, 'update'])
-->name('carrera.update')->where('id','[0-9]+');
- 
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Auth::routes(["register" => false]);
