041b8f7cae1a3d00c4fcdc6b07f7b598a2d8b83c
Karla20Mairena <118698801+Karla20Mairena@users.noreply.github.com>
2023-02-20 13:09:26 -0300

    Clave

---
+++ resources/views/User/clave.blade.php
@@ -0,0 +1,75 @@
+@extends('adminlte::page')
+
+@section('title', 'Cambio contraseña')
+
+@section('content')
+<form action="" method="post">
+        @csrf
+
+    <center><h1>Cambio de contraseña</h1></center>
+    
+    <div class="input-group mb-3">
+            <input type="password" name="viejapassword" class="form-control @error('viejapassword') is-invalid @enderror"
+                   placeholder="contraseña actual">
+
+            <div class="input-group-append">
+                <div class="input-group-text">
+                    <span class="fas fa-lock {{ config('adminlte.classes_auth_icon', '') }}"></span>
+                </div>
+            </div>
+
+            @error('viejapassword')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </div>
+
+    
+
+    {{-- Password field --}}
+        <div class="input-group mb-3">
+            <input type="password" name="password" class="form-control @error('password') is-invalid @enderror"
+                   placeholder="Contraseña nueva">
+
+            <div class="input-group-append">
+                <div class="input-group-text">
+                    <span class="fas fa-lock {{ config('adminlte.classes_auth_icon', '') }}"></span>
+                </div>
+            </div>
+
+            @error('password')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </div>
+
+        {{-- Confirm password field --}}
+        <div class="input-group mb-3">
+            <input type="password" name="password_confirmation"
+                   class="form-control @error('password_confirmation') is-invalid @enderror"
+                   placeholder="{{ __('adminlte::adminlte.retype_password') }}">
+
+            <div class="input-group-append">
+                <div class="input-group-text">
+                    <span class="fas fa-lock {{ config('adminlte.classes_auth_icon', '') }}"></span>
+                </div>
+            </div>
+
+            @error('password_confirmation')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </div>
+
+        {{-- Register button --}}
+        <button type="submit" class="btn btn-block {{ config('adminlte.classes_auth_btn', 'btn-flat btn-primary') }}">
+            <span class="fas fa-user-plus"></span>
+            Guardar cambios
+        </button>
+
+    </form>
+
+@stop
