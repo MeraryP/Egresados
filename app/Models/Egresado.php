24db5d759a770b9d6d0f0bcf27d51ed3e7a5a3c7
Coritza22 <118644032+Coritza22@users.noreply.github.com>
2023-02-20 10:28:42 -0600

    H6

--- app/Models/Egresado.php
+++ app/Models/Egresado.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Egresado extends Model
 {
     use HasFactory;
+    
+    public function carreras(){ 
+
+        return $this->belongsTo( Carrera::class,'carre_id',"id");
+    
+    }
+
+
+    public function genero()
+    {
+        return $this->belongsTo( Genero::class,'gene_id',"id");
+    }
 }
