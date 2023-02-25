b01004ef04e826d0181351069eca6b2d42e628b7
MeraryP <118649149+MeraryP@users.noreply.github.com>
2023-02-25 14:25:32 -0600

    eliminacion

--- resources/views/egresado/index.blade.php
+++ resources/views/egresado/index.blade.php
@@ -92,17 +92,7 @@ setTimeout(quitarerror, 3000);
 
   </table>
 
-   <nav class="main-header navbar navbar-expand navbar-white">
-    <!-- Left navbar links -->
-  
-      <li class="nav-item">
-        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <h1>Editar Registro</h1>
-      </li>
- 
- 
+
 
 {{$egresados->links()}}
 
