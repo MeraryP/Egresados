7bc01e176dca548ca560f3d676bd15195272b77c
Karla20Mairena <118698801+Karla20Mairena@users.noreply.github.com>
2023-03-02 23:10:52 -0300

    Mensaje_Carrera

--- resources/views/Carreras/create.blade.php
+++ resources/views/Carreras/create.blade.php
@@ -10,11 +10,18 @@
     @csrf
 <div class="mb-3">
     <label for="" class="form-label">Nombre de la carrera</label>
-    <input type="text" name="carrera" id="carrera" class="form-control" tabindex="1">
+    <input type="text" value="{{old('carrera')}}"  name="carrera" id="carrera"  class="form-control @error('carrera') is-invalid @enderror"  tabindex="1"
+    title="Ingrese el nombre completo del egresado">
+
+    @error('carrera')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
 </div>
 
 <button type="submit" class="btn btn-primary" tabindex="4"><span class="fas fa-user-plus"></span> Guardar</button>
-<a href="../carreras" class="btn btn-secondary" tabindex="5"><i class="fa fa-times" aria-hidden="true"></i> Cancelar</a>
+<a href="../carreras" class="btn btn-danger" tabindex="5"><i class="fa fa-times" aria-hidden="true"></i> Cancelar</a>
 
 
 </form>
--- resources/views/Carreras/edit.blade.php
+++ resources/views/Carreras/edit.blade.php
@@ -12,10 +12,16 @@
    
 <div class="mb-3">
     <label for="" class="form-label">Nombre de la carrera</label>
-    <input type="text" name="carrera" id="carrera" class="form-control" value="{{ $carrera->Carrera }}">
+    <input type="text" class="form-control @error('carrera') is-invalid @enderror" name="carrera" id="carrera" 
+     value="{{ $carrera->Carrera }}" title="Ingrese el nombre completo del egresado">
+     @error('carrera')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
 </div>
 
 <button type="submit" class="btn btn-primary" tabindex="4"><span class="fas fa-user-plus"></span>Guardar cambios</button>
-<a href="./carreras" class="btn btn-secondary" tabindex="5"><i class="fa fa-times" aria-hidden="true"></i> Cancelar</a>
+<a href="/carreras" class="btn btn-danger" tabindex="5"><i class="fa fa-times" aria-hidden="true"></i> Cancelar</a>
 
 @endsection
